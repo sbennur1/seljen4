2036ebc5936d742ac2a821093f0358fcd7c30e42
sanjiva bennur <sanjiva.bennur@hyniva.com>
2024-12-24 07:56:14 +0530

    jenkin integration

---
+++ src/main/java/selescript/firstscript.java
@@ -0,0 +1,23 @@
+package selescript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.webdriver.*;
+
+public class firstscript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.bing.com/");
+
+	}
+
+}
+
